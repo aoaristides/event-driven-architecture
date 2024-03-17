abbc06a9d8d0b91d47606d29c525ee10d1a823db
Anderson O. Aristides <joyce.miranda@marisa.com.br>
2024-03-17 20:45:09 -0300

    feat: Criado migração do Banco de Customer

---
+++ saga/orchestration/ms-customer/src/main/resources/db/migration/V2__Create_Address.sql
@@ -0,0 +1,16 @@
+CREATE TABLE address
+(
+    id               VARCHAR(255)  NOT NULL PRIMARY KEY,
+    street           VARCHAR(255) NOT NULL,
+    street_number    VARCHAR(255) NOT NULL,
+    complement       VARCHAR(255) NOT NULL,
+    city             VARCHAR(255) NOT NULL,
+    state            VARCHAR(2) NOT NULL,
+    district         VARCHAR(255) NOT NULL,
+    postal_code      VARCHAR(10) NOT NULL,
+    active           BOOLEAN      NOT NULL DEFAULT TRUE,
+    ad_default       BOOLEAN      NOT NULL DEFAULT FALSE,
+    created_at       DATETIME(6)  NOT NULL,
+    updated_at       DATETIME(6)  NOT NULL,
+    deleted_at       DATETIME(6)  NULL
+) ENGINE=InnoDB CHARACTER SET UTF8;
